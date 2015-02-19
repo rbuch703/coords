c9b5fbe233f0d8bfc0dcc54080957d841d9127a2
Robert Buchholz <rbuch703@gmail.com>
2015-02-19 16:15:22 +0100

    - remove outdated tools and dependencies

--- tools/generate_histograms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-#reads a key-value dump, and outputs key-value and key histograms
-
-sort $1 | uniq -c| sort -nr > kv_histogram.csv
-
-python key_freq.py kv_histogram.csv > key_histogram.csv
