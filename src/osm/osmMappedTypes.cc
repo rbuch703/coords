5ff95cb80288323468c0306f6b419d9444b3d2ad
Robert Buchholz <rbuch703@gmail.com>
2015-04-07 11:43:29 +0200

    - added node store class

--- src/osm/osmMappedTypes.cc
+++ src/osm/osmMappedTypes.cc
@@ -409,6 +409,82 @@ void LightweightWayStore::LightweightWayIterator::advanceToNextWay() {
 
 //======================================================
 
+NodeStore::NodeStore(const char* indexFileName, const char* dataFileName) {
+    mapIndex = init_mmap(indexFileName, true, false);
+    mapData  = init_mmap(dataFileName, true, true);
+}
+
+NodeStore::NodeStore(string baseName): 
+    NodeStore( (baseName + ".idx").c_str(), (baseName + ".data").c_str())
+{ }
+
+
+OSMNode NodeStore::operator[](uint64_t nodeId) const
+{
+    uint64_t *index = (uint64_t*)mapIndex.ptr;
+    assert(index[nodeId] != 0 && "trying to access non-existent node");
+    uint64_t dataOffset = index[nodeId];
+    return OSMNode((uint8_t*)mapData.ptr + dataOffset);
+
+}
+
+bool NodeStore::exists(uint64_t nodeId) const
+{
+    uint64_t *index = (uint64_t*)mapIndex.ptr;
+    if (nodeId >= getMaxNumNodes())
+        return false;
+        
+    return index[nodeId] != 0;
+}
+
+NodeStore::NodeIterator NodeStore::begin() 
+{ 
+    return NodeIterator(*this, 0);
+}
+
+NodeStore::NodeIterator NodeStore::end()   
+{ 
+    return NodeIterator(*this, getMaxNumNodes());
+}
+
+uint64_t NodeStore::getMaxNumNodes() const 
+{ 
+    return mapIndex.size / sizeof(uint64_t); 
+}
+
+
+NodeStore::NodeIterator::NodeIterator( NodeStore &host, uint64_t pos): host(host), pos(pos) 
+{
+    advanceToNextNode();
+}
+
+NodeStore::NodeIterator& NodeStore::NodeIterator::operator++() 
+{
+    pos++;
+    advanceToNextNode();
+    return *this;
+}
+
+OSMNode NodeStore::NodeIterator::operator *() 
+{
+    return host[pos];
+}
+
+bool NodeStore::NodeIterator::operator !=( NodeIterator &other) const
+{ 
+    return pos != other.pos;
+} 
+
+
+void NodeStore::NodeIterator::advanceToNextNode() {
+    uint64_t endPos = host.getMaxNumNodes();
+    uint64_t *index = (uint64_t*)host.mapIndex.ptr;
+    while (pos < endPos && index[pos] == 0)
+        pos+=1;            
+}        
+
+//======================================================
+
 RelationStore::RelationStore(const char* indexFileName, const char* dataFileName) {
     mapRelationIndex = init_mmap(indexFileName, true, false);
     mapRelationData  = init_mmap(dataFileName, true, true);
@@ -482,4 +558,3 @@ void RelationStore::RelationIterator::advanceToNextRelation() {
     while (pos < endPos && relationIndex[pos] == 0)
         pos+=1;            
 }        
-
--- src/osm/osmMappedTypes.h
+++ src/osm/osmMappedTypes.h
@@ -195,5 +195,37 @@ private:
 
 };
 
+class NodeStore {
+public:
+    NodeStore(const char* indexFileName, const char* dataFileName);
+    NodeStore(std::string baseName);
+    OSMNode operator[](uint64_t relationId) const;    
+    bool exists(uint64_t relationId) const;
+    uint64_t getMaxNumNodes() const;
+private:
+    class NodeIterator;
+public:
+    
+    NodeIterator begin();
+    NodeIterator end();
+
+private:
+    class NodeIterator {
+    public:
+        NodeIterator( NodeStore &host, uint64_t pos);        
+        NodeIterator& operator++();
+        OSMNode operator *();        
+        bool operator !=( NodeIterator &other) const;    
+    private:
+        void advanceToNextNode();
+        NodeStore &host;
+        uint64_t pos;
+    };
+
+    mmap_t mapIndex;
+    mmap_t mapData;
+
+};
+
 
 #endif
